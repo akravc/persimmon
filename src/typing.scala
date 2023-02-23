8d3b5a8d3f9dd2dd61d23a4ecd117fd3a18ac508
Jonas Iskander <jonasiskander@college.harvard.edu>
2023-02-23 14:51:48 -0500

    Continued implementing typing rules.

--- src/typing.scala
+++ src/typing.scala
@@ -6,6 +6,7 @@ object PersimmonTyping {
         case BExp(b) => BType
         // TODO: Get type of variable from context
         case Var(id) => throw new Exception("Typing of variables not yet implemented.")
-        case 
+        // TODO: Add 'v' to context and get type of 'b' within modified context
+        case Lam(v, t, b) => FunType(t, getType(b))
     }
 }
