c87e04bdc278693982dd26eaa9ade40fd95ee1c5
Jonas Iskander <jonasiskander@college.harvard.edu>
2023-03-10 10:50:05 -0500

    Completed an implementation for the  function.

--- src/typing.scala
+++ src/typing.scala
@@ -1,71 +1,93 @@
 import PersimmonSyntax.*
+import PersimmonLinkages.*
 
 object PersimmonTyping {
     type TypingCtx = Map[String, Type] // Gamma
     type PathCtx = List[Path] // K
     
-    def getType(k: PathCtx, gamma: TypingCtx, e: Expression): Option[Type] = e match {
+    def getType(K: PathCtx, Gamma: TypingCtx, e: Expression): Option[Type] = e match {
         case NExp(n) => Some(NType)
         case BExp(b) => Some(BType)
-        case Var(id) => gamma.get(id) match {
-            case Some(t) => Some(t)
-            case _ => None
-        }
-        case Lam(v, t, b) => {
-            val bt = getType(k, gamma + (v.id -> t), b) match {
-                case Some(t) => t
-                case _ => return None
+        case Var(id) => Gamma.get(id)
+        case Lam(v, t, b) =>
+            getType(K, Gamma + (v.id -> t), b).map {
+                bt => FunType(t, bt)
             }
-            Some(FunType(t, bt))
-        }
-        // TODO
-        case FamFun(path, name) => throw new Exception("Typing of family functions not yet implemented.")
-        case FamCases(path, name) => throw new Exception("Typing of family functions not yet implemented.")
+        case FamFun(path, name) =>
+            computeLinkage(K, path.get).funs.get(name)
+        case FamCases(path, name) =>
+            computeLinkage(K, path.get).cases.get(name).map {
+                (_, ft) => ft
+            }
         case App(e1, e2) => {
-            val (input, output) = getType(k, gamma, e1) match {
-                case Some(FunType(input, output)) => (input, output)
+            val t1 = getType(K, Gamma, e1) match {
+                case Some(t: FunType) => t
                 case _ => return None
             }
-            val t2 = getType(k, gamma, e2) match {
+            val t2 = getType(K, Gamma, e2) match {
                 case Some(t) => t
                 case _ => return None
             }
-            if input == t2 then Some(output)
-            else None
+            if t1.input == t2 then Some(t1.output) else None
         }
         case Rec(fields) => {
-            val option_types = fields.mapValues(field => getType(k, gamma, field)).toMap
-            if option_types.forall((_, t) => t.isEmpty) then None
-            val types = option_types.mapValues(t => t.get).toMap
-            Some(RecType(types))
+            val types = fields.mapValues {
+                field => getType(K, Gamma, field)
+            }.toMap
+            if types.exists((_, t) => t.isEmpty) then None
+            Some(RecType(types.mapValues(t => t.get).toMap))
         }
-        case Proj(e, name) => {
-            val fields = getType(k, gamma, e) match {
-                case Some(RecType(fields)) => fields
+        case Proj(e, name) => getType(K, Gamma, e).flatMap {
+            case RecType(fields) => fields.get(name)
+            case _ => None
+        }
+        case Inst(t, rec) => {
+            val fields = computeLinkage(K, t.path.get).types.get(t.name) match {
+                case Some(typeDefn) => typeDefn.typeBody.defn.get.fields
                 case _ => return None
             }
-            fields.get(name) match {
-                case Some(field) => Some(field)
-                case _ => return None
-            }
+            if fields.keySet != rec.fields.keySet then return None
+            if fields.forall(
+                (name, t) => getType(K, Gamma, rec.fields.get(name).get) == Some(t)
+            ) then Some(t) else None
         }
         // TODO: Do some extra checks.
-        case Inst(t, rec) => Some(t)
-        // TODO: Do some extra checks.
-        case InstADT(t, cname, rec) => Some(t)
-        case Match(e, c, r) => throw new Exception("Typing of match expressions not yet implemented.")
-        case IfThenElse(condExpr, ifExpr, elseExpr) => {
-            if getType(k, gamma, condExpr) != BType then return None
-            val ifType = getType(k, gamma, ifExpr) match {
+        case InstADT(t, cname, rec) =>
+            val cases = computeLinkage(K, t.path.get).adts.get(t.name) match {
+                case Some(adtDefn) => adtDefn.adtBody.defn.get
+                case _ => return None
+            }
+            val fields = cases.get(cname) match {
+                case Some(rec) => rec.fields
+                case _ => return None
+            }
+            if fields.keySet != rec.fields.keySet then return None
+            if fields.forall(
+                (name, t) => getType(K, Gamma, rec.fields.get(name).get) == Some(t)
+            ) then Some(t) else None
+        case Match(e, c, r) =>
+            val t = getType(K, Gamma, e) match {
+                case Some(t: PathType) => t
+                case _ => return None
+            }
+            val funType = computeLinkage(K, c.path.get).cases.get(c.name) match {
+                case Some(pathType, funType) =>
+                    if pathType != t then return None
+                    funType
+                case _ => return None
+            }
+            if getType(K, Gamma, r) != Some(funType.input) then return None
+            Some(funType.output)
+        case IfThenElse(condExpr, ifExpr, elseExpr) =>
+            if getType(K, Gamma, condExpr) != BType then return None
+            val ifType = getType(K, Gamma, ifExpr) match {
                 case Some(t) => Some(t)
                 case _ => return None
             }
-            val elseType = getType(k, gamma, elseExpr) match {
+            val elseType = getType(K, Gamma, elseExpr) match {
                 case Some(t) => Some(t)
                 case _ => return None
             }
-            if ifType != elseType then return None
-            ifType
-        }
+            if ifType == elseType then ifType else None
     }
 }
