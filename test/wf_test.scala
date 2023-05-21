0fdaf10b9d2703b8e4ae0a741c3e453e53fba32b
Nada Amin <namin@seas.harvard.edu>
2023-05-20 23:53:37 -0400

    add stlc0

--- test/wf_test.scala
+++ test/wf_test.scala
@@ -140,6 +140,12 @@ class WFTesting extends AnyFunSuite {
     //     var lkg = computeDefLinkage(prog)
     //     assertResult(wfDef(List(Prog), lkg)){ true }
     // }
+    test("wf - ex: stlc0") {
+        val p = readFile("res/stlc0")
+        PersimmonLinkages.p = p
+        var lkg = computeDefLinkage(prog)
+        assertResult(wfDef(List(Prog), lkg)){ true }
+    }
     // test("wf - ex: stlc") {
     //     val p = readFile("res/stlc")
     //     PersimmonLinkages.p = p
